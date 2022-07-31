4938dc8f025c243371efdbeb68435604ef3af4ef
geraldwogan <geraldwogan@gmail.com>
2022-07-31 10:54:11 +0100

    feat: add recursion to repeatably try the next number until 495 is the result

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 from random import randint
 
-def get_nums():
-    # initialise num
-    num = ''
-
-    # add rand int between 0-9 three times.
-    for x in range(0,3):
-        digit = str(randint(0,9))
-        num += digit
-    
+def sort_and_reverse(num):
     # sorting nums
     num = ''.join(sorted(num, reverse=True))
     reverse_num = ''.join(sorted(num))
-    
-    # repeat num check
+
+    return num, reverse_num
+
+def get_nums_recurse(num):
+    num, reverse_num = sort_and_reverse(num)
+
+    # repeated num check
     if num[0] == num[1] and num[1] == num[2]:
         print('error : {num}')
         return '1234'
 
-    return int(num), int(reverse_num)
+    print(num, '-', reverse_num)
 
-def subtraction_recursion(num, reverse_num):
-    result = num - reverse_num
-    
+    result = int(num) - int(reverse_num)
+
     if result == 495:
         print(f"success! {result} found.")
+        return
     else:
         print(f"failure. result is {result}.")
+        get_nums_recurse(str(result))
 
-num, reverse_num = get_nums()
-print(num, reverse_num)
-subtraction_recursion(num, reverse_num)
+# initialise num
+num = ''
+
+# add rand int between 0-9 three times.
+for x in range(0,3):
+    digit = str(randint(0,9))
+    num += digit
+
+get_nums_recurse(num)
