cb874793a13eff9458b222cdff3936e773657ac5
geraldwogan <geraldwogan@gmail.com>
2022-08-01 15:46:08 +0100

    feat: add function for returning a three digit number as a string

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_nums_recurse(num):
 
     # repeated num check
     if num[0] == num[1] and num[1] == num[2]:
-        print('error : {num}')
+        print(f'error : {num}')
         return '1234'
 
     print(num, '-', reverse_num)
@@ -26,12 +26,16 @@ def get_nums_recurse(num):
         print(f"failure. result is {result}.")
         get_nums_recurse(str(result))
 
-# initialise num
-num = ''
+def get_3_digit_num_as_str():
+    num = ''
 
-# add rand int between 0-9 three times.
-for x in range(0,3):
-    digit = str(randint(0,9))
-    num += digit
+    # # add rand int between 0-9 three times.
+    for x in range(0,3):
+        digit = str(randint(0,9))
+        num += digit
+
+    return num
+
+num = get_3_digit_num_as_str()
 
 get_nums_recurse(num)
