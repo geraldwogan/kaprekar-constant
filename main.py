f8e56b4a732756fab15fa7f057989ec8b151e2ec
geraldwogan <geraldwogan@gmail.com>
2022-08-01 16:00:01 +0100

    feat: add recursion to ensure the original 3 digit number will be non-repeating

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from random import randint
 
+def get_3_digit_non_repeating_num():
+    num = ''
+
+    # # add rand int between 0-9 three times.
+    for x in range(0,3):
+        digit = str(randint(0,9))
+        num += digit
+    
+    # repeated num check
+    while num[0] == num[1] and num[1] == num[2]:
+        print(f'error : {num}')
+        num = get_3_digit_non_repeating_num()
+
+    return num
+
 def sort_and_reverse(num):
     # sorting nums
     num = ''.join(sorted(num, reverse=True))
@@ -9,11 +24,6 @@ def sort_and_reverse(num):
 
 def get_nums_recurse(num):
     num, reverse_num = sort_and_reverse(num)
-
-    # repeated num check
-    if num[0] == num[1] and num[1] == num[2]:
-        print(f'error : {num}')
-        return '1234'
 
     print(num, '-', reverse_num)
 
@@ -26,16 +36,6 @@ def get_nums_recurse(num):
         print(f"failure. result is {result}.")
         get_nums_recurse(str(result))
 
-def get_3_digit_num_as_str():
-    num = ''
-
-    # # add rand int between 0-9 three times.
-    for x in range(0,3):
-        digit = str(randint(0,9))
-        num += digit
-
-    return num
-
-num = get_3_digit_num_as_str()
+num = get_3_digit_non_repeating_num()
 
 get_nums_recurse(num)
