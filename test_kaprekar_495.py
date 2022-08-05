74c7232c6a494d99644c26b0169242ec15a50c4c
geraldwogan <geraldwogan@gmail.com>
2022-08-05 11:36:02 +0100

    test: add test for both return values of the sort_and_reverse() function

--- test_kaprekar_495.py
+++ test_kaprekar_495.py
@@ -1,16 +1,10 @@
-import unittest
-# import main.kaprekar_495 as kap
 from kaprekar_495 import * 
-
-# class TestKaprekar495(unittest.TestCase):
-
-#     def test_get_3_digit_non_repeating_num(self):
-#         return_num = get_3_digit_non_repeating_num()
-#         self.assertEqual(len(return_num), 3)
-
-# unittest.main()
 
 def test_get_3_digit_non_repeating_num():
     return_num = get_3_digit_non_repeating_num()
     assert len(return_num) == 3
 
+def test_sort_and_reverse():
+    num, reversed_num = sort_and_reverse('869')
+    assert num == '986'
+    assert reversed_num == '689'
