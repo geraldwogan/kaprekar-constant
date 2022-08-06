9301a161aadbb7e5cb845df712562e447485ed81
geraldwogan <geraldwogan@gmail.com>
2022-08-06 01:44:23 +0100

    improve 3_digit...() test to check for non-repeating number

--- test_kaprekar_495.py
+++ test_kaprekar_495.py
@@ -3,6 +3,7 @@ from kaprekar_495 import *
 def test_get_3_digit_non_repeating_num():
     return_num = get_3_digit_non_repeating_num()
     assert len(return_num) == 3
+    assert (return_num[0] != return_num[1]) & (return_num[0] != return_num[2])
 
 def test_sort_and_reverse():
     num, reversed_num = sort_and_reverse('869')
