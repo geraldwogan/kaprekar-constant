3798283ae2b7b5535f74c5840fd56a1310a17a0a
geraldwogan <geraldwogan@gmail.com>
2022-08-06 01:45:30 +0100

    test: add test for get_3_digit...() function()

--- test_kaprekar_6174.py
+++ test_kaprekar_6174.py
@@ -1,1 +1,6 @@
-import kaprekar_6174 as kap
+from kaprekar_6174 import *
+
+def test_get_3_digit_non_repeating_num():
+    return_num = get_4_digit_non_repeating_num()
+    assert len(return_num) == 4
+    assert (return_num[0] != return_num[1]) & (return_num[0] != return_num[2]) & (return_num[0] != return_num[3])
