944d782aeb256049572f7c3556e53b8068818e37
geraldwogan <geraldwogan@gmail.com>
2022-08-06 01:47:41 +0100

    test: add test for sort_and_reverse() function

--- test_kaprekar_6174.py
+++ test_kaprekar_6174.py
@@ -3,4 +3,9 @@ from kaprekar_6174 import *
 def test_get_3_digit_non_repeating_num():
     return_num = get_4_digit_non_repeating_num()
     assert len(return_num) == 4
-    assert (return_num[0] != return_num[1]) & (return_num[0] != return_num[2]) & (return_num[0] != return_num[3])
+    assert ((return_num[0] != return_num[1]) & (return_num[0] != return_num[2]) & (return_num[0] != return_num[3]))
+
+def test_sort_and_reverse():
+    num, reversed_num = sort_and_reverse('8692')
+    assert num == '9862'
+    assert reversed_num == '2689'
